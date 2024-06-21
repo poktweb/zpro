9044d8161984fbb25f655421a6f4907ed4507085
Marcus Vieira <marcus.vieiraleal94@gmail.com>
2024-06-20 21:21:20 -0300

    aaa

---
+++ zpro_passaporte_shell/lib/manifest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source "${PROJECT_ROOT}"/lib/_backend.sh
+source "${PROJECT_ROOT}"/lib/_frontend.sh
+source "${PROJECT_ROOT}"/lib/_inquiry.sh
+source "${PROJECT_ROOT}"/lib/_migration.sh
+source "${PROJECT_ROOT}"/lib/_pendingfix.sh
+source "${PROJECT_ROOT}"/lib/_system.sh
+source "${PROJECT_ROOT}"/lib/_update.sh
+source "${PROJECT_ROOT}"/lib/_wwebjs.sh
